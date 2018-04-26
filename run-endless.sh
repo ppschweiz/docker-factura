6d0f4824889bf095f532857687c95d2f743bb708
Stefan Thoeni <stefan.thoeni@piratenpartei.de>
2018-04-26 23:01:56 +0200

    Added stats every monday

--- run-endless.sh
+++ run-endless.sh
@@ -6,6 +6,17 @@ do
 
 	./run-facturer.sh
 
+	DOW=$(date +%u)
+	HOUR=$(date +%H)
+
+	if [ $DOW -eq 1 ]
+	then
+		if [ $HOUR -gt 19 ]
+		then
+			./run-stats.sh
+		fi
+	fi
+
         for i in `seq 1 24`;
         do
         	for i in `seq 1 30`;
